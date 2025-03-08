955e88990dbea46c5e4ec3f9f7bb8606d8b2c122
qchien643 <nguyenquanhchienpp2303@gmail.com>
2025-03-09 00:39:18 +0700

    no frontend directory

---
+++ backend/main.py
@@ -0,0 +1,87 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS  # Import the CORS class
+import json 
+
+app = Flask(__name__)
+CORS(app, resources={r"/api/*": {"origins": "*"}}, supports_credentials=True, methods=["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"])
+
+# default prompt = tôi muốn xây phòng ngủ với một chiếc bàn hiện đại , một chiếc giường mới và một chiếc tủ to
+
+# NOTE : format of data from frontend""""
+#      {
+#         flag : flag ,
+#         data : data
+#     }
+# ""
+
+"""
+ flag sending data from chatbot : "message"
+ flag sending data from user : "flowchart"
+ 
+ +format base on flag:
+ 
+ -flag : "message" 
+ --> {
+        flag : <flag>,
+        data : {
+            prompt : <prompt>
+        }
+    }
+
+-flag : "flowchart"
+-->{
+        flag : <flag>,
+        data : {
+            "room": {
+                "floor": "tile",
+                "wall": "brick",
+                "dimension": [
+                "5",
+                "5",
+                "5"
+                ]
+            },
+            "furnitures": [
+                {
+                "type": "chair",
+                "model": "gaming_chair",
+                "placement": "BehindFurnitureRule table [] center | SameDirection table []"
+                },
+                {
+                "type": "bed",
+                "model": "bed_1",
+                "placement": "NextToWallRule None ['E', 'S'] left | SameDirectionWall None ['E', 'S']"
+                }
+            ]
+        }
+    }
+"""
+
+# stated-based function dispatch
+def extract_prompt(prompt):
+    pass
+
+def dispatcher(datas):
+    flag , data = datas["flag"] , datas["data"]
+    if flag == 'message':
+        # câu hỏi của người dùng sẽ có các trạng thái : yêu cầu , sửa đổi , chưa rõ 
+        # đơn giản hóa trường hợp ở sau chỉ là yêu cầu
+        promptUser = data["message"]
+        return extract_prompt(promptUser)
+
+
+@app.route('/api/chat', methods=['GET', 'POST'])
+def chat():
+    try:
+        data = request.get_json()
+        res = dispatcher(data)  
+        return jsonify(res)
+
+    except Exception as e:
+        print(f"Error processing request: {str(e)}")
+        return jsonify({'error': 'Internal Server Error'}), 500
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
